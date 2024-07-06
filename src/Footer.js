ae0cc787b662fd19bcca5b45e5a632e625edc544
Nimeshan Shanmugam <admin@Nimeshans-MacBook-Air.local>
2024-07-06 18:36:50 +0530

    Initial Commit

---
+++ src/Footer.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Footer.css";
+import {
+  PlayCircleOutline,
+  SkipPrevious,
+  SkipNext,
+  PlaylistPlay,
+  Shuffle,
+  Repeat,
+  VolumeDown,
+} from "@material-ui/icons";
+import { Grid, Slider } from "@material-ui/core";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img
+          src="https://cdn.stocksnap.io/img-thumbs/960w/dock-lake_U94B4HVBAO.jpg"
+          alt=""
+          className="footer__albumLogo"
+        />
+        <div className="footer__songInfo">
+          <h4>My Song 01</h4>
+          <p>Nimeshan</p>
+        </div>
+      </div>
+      <div className="footer__center">
+        <Shuffle className="footer__green" />
+        <SkipPrevious className="footer__icon" />
+        <PlayCircleOutline fontSize="large" className="footer__icon" />
+        <SkipNext className="footer__icon" />
+        <Repeat className="footer__green" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlay />
+          </Grid>
+          <Grid item>
+            <VolumeDown />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
